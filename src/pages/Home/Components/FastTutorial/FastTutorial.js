433552ee09460226f60daebcbdfaa94ea0edabef
João Vitor <joaovitor@iMac-de-Joao.local>
2020-05-28 13:59:10 -0300

    Remove TouchableOpacity fasttutorial

--- src/pages/Home/Components/FastTutorial/FastTutorial.js
+++ src/pages/Home/Components/FastTutorial/FastTutorial.js
@@ -1,31 +1,29 @@
-import React, {useRef, useState, useEffect} from 'react';
-import {View, Text, Dimensions, TouchableOpacity} from 'react-native';
+import React, {useRef} from 'react';
+import {View, Text, Dimensions} from 'react-native';
 import YoutubePlayer from 'react-native-youtube-iframe';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import {styles} from './styles';
 
 const FastTutorial = () => {
-    const [played, setPlayed] = useState(false);
     const width = Dimensions.get('window').width;
     const player1Ref = useRef(null);
     const player2Ref = useRef(null);
 
     return (
         <View>
-            <TouchableOpacity onPress={() => setPlayed(!played)}>
-                <View style={styles.titleContainer}>
-                    <Icon name="video-library" size={30} color="#999" />
-                    <Text style={styles.titleText}> Fast Tutorial </Text>
-                </View>
-            </TouchableOpacity>
+            <View style={styles.titleContainer}>
+                <Icon name="video-library" size={30} color="#999" />
+                <Text style={styles.titleText}> Fast Tutorial </Text>
+            </View>
+
             <View style={styles.videoContainer}>
                 <YoutubePlayer
                     ref={player1Ref}
                     height={width * 0.9 * 0.56} //the height youtube video is 56% of width
                     width={width * 0.9}
                     videoId={'kbGCnu39YGM'}
-                    play={played}
+                    play={false}
                     volume={50}
                     playbackRate={1}
                     playerParams={{
